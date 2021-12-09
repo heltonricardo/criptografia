2b90d2ff4f1cee2321c6e582a9a8b36783b6757c
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-12-08 22:38:23 -0300

    cifra de cezar utf8

---
+++ cezar/cifra.ts
@@ -0,0 +1,13 @@
+const deslocamento: number = 7;
+
+const converterUTF = (c: string): string =>
+  c !== " " ? String.fromCharCode((c.charCodeAt(0) + deslocamento) % 255) : c;
+
+const cifrar = (msg: string): string =>
+  Array.from(msg).map(converterUTF).join("");
+
+const msg1: string = "A César o que é de César.";
+const msg2: string = "Todos os caminhos levam a Roma.";
+
+console.log(cifrar(msg1));
+console.log(cifrar(msg2));
