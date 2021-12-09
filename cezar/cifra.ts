5ae12e4b3c8661f3c09a715400f7bff8aaeef4d5
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-12-08 22:46:51 -0300

    correcao de case de constantes

--- cezar/cifra.ts
+++ cezar/cifra.ts
@@ -1,4 +1,6 @@
 const DESLOCAMENTO: number = 7;
+const MSG1: string = "A César o que é de César.";
+const MSG2: string = "Todos os caminhos levam a Roma.";
 
 const converterUTF = (c: string): string =>
   c !== " " ? String.fromCharCode((c.charCodeAt(0) + DESLOCAMENTO) % 255) : c;
@@ -6,8 +8,5 @@ const converterUTF = (c: string): string =>
 const cifrar = (msg: string): string =>
   Array.from(msg).map(converterUTF).join("");
 
-const msg1: string = "A César o que é de César.";
-const msg2: string = "Todos os caminhos levam a Roma.";
-
-console.log(cifrar(msg1));
-console.log(cifrar(msg2));
+console.log(cifrar(MSG1));
+console.log(cifrar(MSG2));
