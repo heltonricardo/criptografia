ada22ab952835fb0c89a0e46008f4ab634825b84
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-12-09 22:00:02 -0300

    alterando nomenclaturas

--- caesar/cifra.ts
+++ caesar/cifra.ts
@@ -2,11 +2,10 @@ const DESLOCAMENTO: number = 7;
 const MSG1: string = "A César o que é de César.";
 const MSG2: string = "Todos os caminhos levam a Roma.";
 
-const converterUTF = (c: string): string =>
+const converter = (c: string): string =>
   c !== " " ? String.fromCharCode((c.charCodeAt(0) + DESLOCAMENTO) % 255) : c;
 
-const cifrar = (msg: string): string =>
-  Array.from(msg).map(converterUTF).join("");
+const cifrar = (msg: string): string => Array.from(msg).map(converter).join("");
 
 console.log(cifrar(MSG1));
 console.log(cifrar(MSG2));
