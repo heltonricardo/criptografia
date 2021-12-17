f11aa8d42cd2700ed470b70d351e02e535f4590d
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-12-16 22:01:43 -0300

    Update decifra.ts

--- caesar/decifra.ts
+++ caesar/decifra.ts
@@ -1,63 +1,63 @@
 class Decifra {
-  private frequencia: Map<string, number> = new Map();
+  private mensagemOriginal: string;
+  private mensagemDecifrada: string = "";
+  private mensagemSanitarizada: string = "";
 
-  constructor() {
-    Array.from(new Array(26).keys()).forEach((p) => {
-      this.frequencia.set(this.codigoParaLetra(p), 0);
-    });
+  private readonly DESLOCAMENTO: number = 7;
+
+  constructor(mensagemOriginal: string) {
+    this.mensagemOriginal = mensagemOriginal;
+    this.mensagemSanitarizada = this.sanitarizar(this.mensagemOriginal);
+    this.mensagemDecifrada = this.decifrar();
   }
 
-  /***************************** MÉTODOS PRIVADOS *****************************/
+  public getMensagemOriginal(): string {
+    return this.mensagemOriginal;
+  }
 
-  private sanitarizar(msg: string): string {
-    return msg
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .toUpperCase()
-      .replace(/[^A-Z]/g, "");
+  public getMensagemSanitarizada(): string {
+    return this.mensagemSanitarizada;
+  }
+
+  public getMensagemDecifrada(): string {
+    return this.mensagemDecifrada;
+  }
+
+  private letraParaCodigo(c: string): number {
+    return c.charCodeAt(0) - 65;
   }
 
   private codigoParaLetra(n: number): string {
     return String.fromCharCode(n + 65);
   }
 
-  private getFrequenciaPorLetra(c: string): number {
-    return this.frequencia.get(c) ?? 0;
+  private sanitarizar(msg: string): string {
+    return msg
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toUpperCase();
   }
 
-  private incrementarFrequencia(letra: string) {
-    this.frequencia.set(letra, this.getFrequenciaPorLetra(letra) + 1);
-  }
+  private converter = (c: string): string =>
+    c.search(/[A-Z]/) != -1
+      ? this.codigoParaLetra(
+          (this.letraParaCodigo(c) - this.DESLOCAMENTO + 26) % 26
+        )
+      : c;
 
-  /***************************** MÉTODOS PÚBLICOS *****************************/
-
-  public treinar(texto: string): void {
-    Array.from(this.sanitarizar(texto)).forEach(
-      this.incrementarFrequencia,
-      this
-    );
-  }
-
-  public converter(c: string): string {
-    return c !== " " ? String.fromCharCode((c.charCodeAt(0) + 1) % 255) : c;
-  }
-
-  public decifrar = (msg: string): string =>
-    Array.from(msg).map(this.converter).join("");
+  private decifrar = (): string =>
+    Array.from(this.mensagemSanitarizada).map(this.converter).join("");
 }
 
-const EXEMPLO: string = `O Brasil é considerado um país grande. Dessa forma,
-possui uma extensa variedade cultural, algo que pode ser percebido através
-das músicas que são lançadas todos os anos. Os ritmos musicais são muito
-variados, assim como os cantores e compositores, fazendo sucesso dentro e
-fora do país. Há alguns cantores que, mesmo tendo interpretado músicas no
-passado, ainda são lembrados pelo público. Um desses cantores é o Caetano
-Veloso, que possui uma grande obra musical.`;
+/***************************** TESTE DO ALGORITMO *****************************/
 
-const MENSAGEM: string =
-  "UVCBGRFR URHEVFGVPN R NDHRYN DHR PBZB ZRGBQB QR GENONYUB R NQBGNQN N " +
-  "GVGHYB CEBIVFBEVB PBZB VQRVN QVERGEVM AN CRFDHVFN QBF SNGBF FRZ N " +
-  "CERBPHCNPNB VAVPVNY QR FHN IREQNQR BH SNYFVQNQR";
+const msg_decifrada: string =
+  "H JLZHY V XBL L KL JLZHY. AVKVZ VZ JHTPUOVZ SLCHT H YVTH.";
 
-const decifra = new Decifra();
-decifra.treinar(EXEMPLO);
+const decifra = new Decifra(msg_decifrada);
+
+console.log(decifra.getMensagemOriginal());
+console.log(decifra.getMensagemSanitarizada());
+console.log(decifra.getMensagemDecifrada());
+
+/******************************************************************************/
