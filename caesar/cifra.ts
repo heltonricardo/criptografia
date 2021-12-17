2fd6fc3861553b6c2ee7973c1b916ce2ce210c24
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-12-16 21:51:53 -0300

    Update cifra.ts

--- caesar/cifra.ts
+++ caesar/cifra.ts
@@ -1,11 +1,54 @@
-const DESLOCAMENTO: number = 7;
-const MSG1: string = "A César o que é de César.";
-const MSG2: string = "Todos os caminhos levam a Roma.";
+class Cifra {
+  private mensagemOriginal: string;
+  private mensagemCifrada: string = "";
+  private mensagemSanitarizada: string = "";
 
-const converter = (c: string): string =>
-  c !== " " ? String.fromCharCode((c.charCodeAt(0) + DESLOCAMENTO) % 255) : c;
+  private readonly DESLOCAMENTO: number = 7;
 
-const cifrar = (msg: string): string => Array.from(msg).map(converter).join("");
+  constructor(mensagemOriginal: string) {
+    this.mensagemOriginal = mensagemOriginal;
+    this.mensagemSanitarizada = this.sanitarizar(this.mensagemOriginal);
+    this.mensagemCifrada = this.cifrar();
+  }
 
-console.log(cifrar(MSG1));
-console.log(cifrar(MSG2));
+  public getMensagemOriginal(): string {
+    return this.mensagemOriginal;
+  }
+
+  public getMensagemSanitarizada(): string {
+    return this.mensagemSanitarizada;
+  }
+
+  public getMensagemCifrada(): string {
+    return this.mensagemCifrada;
+  }
+
+  private letraParaCodigo(c: string): number {
+    return c.charCodeAt(0) - 65;
+  }
+
+  private codigoParaLetra(n: number): string {
+    return String.fromCharCode(n + 65);
+  }
+
+  private sanitarizar(msg: string): string {
+    return msg
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toUpperCase();
+  }
+
+  private converter = (c: string): string =>
+    c.search(/[A-Z]/) != -1
+      ? this.codigoParaLetra((this.letraParaCodigo(c) + this.DESLOCAMENTO) % 26)
+      : c;
+
+  private cifrar = (): string =>
+    Array.from(this.mensagemSanitarizada).map(this.converter).join("");
+}
+
+const msg: string = "A César o que é de César. Todos os caminhos levam à Roma.";
+const cifra = new Cifra(msg);
+console.log(cifra.getMensagemOriginal());
+console.log(cifra.getMensagemSanitarizada());
+console.log(cifra.getMensagemCifrada());
