a795be2536d987f70dd68582fab0484385ef1d73
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-12-16 22:18:08 -0300

    Update tabela.ts

--- caesar/tabela.ts
+++ caesar/tabela.ts
@@ -5,30 +5,38 @@ function exibirHeader(): void {
   console.log("-".repeat(msg.length));
 }
 
+const numFormat = (n: number): string => String(n).padStart(2);
+
 function centralizar(msg: string, tamanho: number): string {
   const emBranco = tamanho - msg.length;
-  const frente = Number.parseInt((emBranco / 2).toString());
-  const tras = emBranco - frente;
+  const tras = Number.parseInt((emBranco / 2).toString());
+  const frente = emBranco - tras;
   return " ".repeat(frente) + msg + " ".repeat(tras);
 }
 
 function exibirLinha(letra: string): void {
   const posicao: number = letra.charCodeAt(0) - 65;
-  const somaAlgebrica: string = `${posicao} + 3 = ${posicao + 3}`;
-  const somaModulo: string = `(${posicao} + 3) MOD 26 = ${(posicao + 3) % 26}`;
+  const somaA: string = `${numFormat(posicao)} + 3 = ${numFormat(posicao + 3)}`;
+  const somaM: string = `(${numFormat(posicao)} + 3) MOD 26 = ${numFormat(
+    (posicao + 3) % 26
+  )}`;
   const novaLetra: string = String.fromCharCode(((posicao + 3) % 26) + 65);
   const msg: string =
     centralizar(letra, 6) +
     "|" +
-    centralizar(posicao.toString(), 9) +
+    centralizar(numFormat(posicao), 9) +
     "|" +
-    centralizar(somaAlgebrica, 16) +
+    centralizar(somaA, 16) +
     "|" +
-    centralizar(somaModulo, 23) +
+    centralizar(somaM, 22) +
     "|" +
     centralizar(novaLetra, 10);
   console.log(msg);
 }
 
+/***************************** TESTE DO ALGORITMO *****************************/
+
 exibirHeader();
 [...Array(26)].map((_, i) => String.fromCharCode(i + 65)).forEach(exibirLinha);
+
+/******************************************************************************/
